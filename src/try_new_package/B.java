3fd94efe1f009b1124d38497c74585293b6fc54b
Tusharnm <tushar.05masne@gmail.com>
2023-09-17 06:51:37 +0530

    inside folder and able to see src and other folder

---
+++ src/try_new_package/B.java
@@ -0,0 +1,26 @@
+package try_new_package;
+
+public class B {
+	
+		public static void main(String[] Tush){
+
+		 System.out.println("Hello");
+		B i=new B();
+		i.m1();
+
+		}
+
+		public void m1(){
+		 int a=23;
+		int b=43;
+		int c;
+		c=a+b;
+		System.out.print(c);
+
+		}
+
+
+
+		}
+
+
